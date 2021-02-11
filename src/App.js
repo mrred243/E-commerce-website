9490a47efbfd26d5047d360f5d633cd4d97cc302
mrred243 <tranthie04@gmail.com>
2021-02-11 10:58:36 +0200

    Add products from  commercejs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { Products, Navbar } from './components/index'
+import { commerce } from './lib/commerce'
 
 const App = () => {
+    const [ products, setProducts ] = useState([]);
+    
+    const fetchProducts = async () => {
+        const { data } = await commerce.products.list();
+        setProducts(data);
+    }
+
+    useEffect(() => {
+        fetchProducts();
+    }, [])
+
+    console.log(products)
+
     return (
         <div>
             <Navbar />
-            <Products />
+            <Products products={products} />
         </div>
     )
 }
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,16 +4,12 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product'
 import useStyles from './styles' 
 
-const products =[
-    { id: 1, name: 'Shoes', description: 'Running shoes.', price: '$5' },
-    { id: 2, name: 'Macbook', description: 'wonderful macbook.', price: '$10' },
-
-];
 
 
 
 
-const Products = () => {
+
+const Products = ({ products }) => {
     const classes = useStyles()
     
     return(
