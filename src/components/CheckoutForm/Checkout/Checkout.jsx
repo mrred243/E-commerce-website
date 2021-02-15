d9f7b9a0953dc88c4b91916bc964b7fa089b06f4
mrred243 <tranthie04@gmail.com>
2021-02-15 18:55:30 +0200

    Set up functions in  address form

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -12,6 +12,7 @@ const Checkout = ({ cart }) => {
     const classes = useStyles();
     const [ activeStep, setActiveStep ] = useState(0);
     const [ checkoutToken, setCheckoutToken] = useState(null)
+    const [ shippingData, setShippingData ] = useState({})
 
     useEffect(() => {
         if (cart.id) {
@@ -27,6 +28,15 @@ const Checkout = ({ cart }) => {
             generateToken();
         }
     }, [cart])
+
+    const nextStep = () => setActiveStep((prevActiveStep) => prevActiveStep + 1)
+    const backStep = () => setActiveStep((prevActiveStep) => prevActiveStep - 1)
+
+    const next = (data) => {
+        setShippingData(data)
+        nextStep()
+    }
+
     const Confirmation = () => (
         <div>
             Confirmation
@@ -34,8 +44,8 @@ const Checkout = ({ cart }) => {
     )
 
     const Form = () => activeStep === 0
-        ? <AddressForm checkoutToken={checkoutToken} />
-        : <PaymentForm />
+        ? <AddressForm checkoutToken={checkoutToken} next={next} />
+        : <PaymentForm shippingData={shippingData} />
     
         return (
         <>
