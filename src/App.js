60c7072560302b94c30a96c612b27d69ca879d92
mrred243 <tranthie04@gmail.com>
2021-02-15 14:34:25 +0200

    set up checkout page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-import { Products, Navbar, Cart } from './components/index'
+import { Products, Navbar, Cart, Checkout } from './components/index'
 import { commerce } from './lib/commerce'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom' 
 const App = () => {
@@ -57,6 +57,9 @@ const App = () => {
                             handleRemoveFromCart={handleRemoveFromCart}
                             handleEmptyCart={handleEmptyCart}
                         />
+                    </Route>
+                    <Route exact path='/checkout'>
+                        <Checkout  />
                     </Route>
                 </Switch>
             </div>
